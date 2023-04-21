c10e8e66de5673cd4bd965cadae5cc5a8c6ef18f
Mark Tyneway <mark.tyneway@gmail.com>
2023-04-21 04:04:19 -0700

    cleanup

--- src/ZuzaluOracle.sol
+++ src/ZuzaluOracle.sol
@@ -1,56 +1,112 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-import { SemaphoreVerifier } from "./SemaphoreVerifier.sol";
+import { SemaphoreVerifier } from "@semaphore-protocol/contracts/base/SemaphoreVerifier.sol";
 import { Owned } from "solmate/auth/Owned.sol";
 
-/*
-function register(address, proof) external {
-  if (ORACLE.verify(proof)) {
-    residents[address] = true
-  }
-}
-
-modifier onlyResident() {
-  ...
-}
-*/
-
+/// @title
 contract ZuzaluOracle is Owned {
+    /// @notice
+    struct Commitment {
+        uint256 root;
+        uint256 depth;
+    }
+
+    /// @notice
     event Update(uint256 root, uint256 depth);
 
-    uint256 public root;
-    uint256 public depth;
+    /// @notice
+    error NotCanonical(uint256);
+
+    /// @notice
+    Commitment private _latestCommitment;
+
+    /// @notice root -> depth
+    mapping(uint256 => uint256) public commitments;
+
+    /// @notice
     address immutable public VERIFIER;
 
-    constructor(address _owner, uint256 _root, uint256 _depth, address _verifier) Owned(_owner) {
+    /// @notice
+    constructor(Commitment memory _commitment, address _owner, address _verifier) Owned(_owner) {
         VERIFIER = _verifier;
-        _update(_root, _depth);
+        _update(_commitment);
     }
 
-    function update(uint256 _root, uint256 _depth) onlyOwner public {
-        _update(_root, _depth);
+    /// @notice
+    function commitment() public view returns (Commitment memory) {
+        return _latestCommitment;
     }
 
-    function _update(uint256 _root, uint256 _depth) internal {
-        root = _root;
-        depth = _depth;
-        emit Update(_root, depth);
+    /// @notice
+    function update(Commitment memory _commitment) onlyOwner public {
+        _update(_commitment);
     }
 
+    /// @notice
+    function _update(Commitment memory _commitment) internal {
+        _latestCommitment = _commitment;
+        commitments[_commitment.root] = _commitment.depth;
+        emit Update(_commitment.root, _commitment.depth);
+    }
+
+    /// @notice
     function verify(
         uint256 _nullifierHash,
         uint256 _signal,
         uint256 _externalNullifier,
         uint256[8] calldata _proof
-    ) external returns (bool) {
+    ) external view returns (bool) {
+        uint256 root = _latestCommitment.root;
+        uint256 depth = _latestCommitment.depth;
+
+        return _verify({
+            _root: root,
+            _depth: depth,
+            _nullifierHash: _nullifierHash,
+            _signal: _signal,
+            _externalNullifier: _externalNullifier,
+            _proof: _proof
+        });
+    }
+
+    /// @notice
+    function verify(
+        uint256 _root,
+        uint256 _nullifierHash,
+        uint256 _signal,
+        uint256 _externalNullifier,
+        uint256[8] calldata _proof
+    ) external view returns (bool) {
+        uint256 depth = commitments[_root];
+        if (depth == 0) revert NotCanonical(_root);
+
+        return _verify({
+            _root: _root,
+            _depth: depth,
+            _nullifierHash: _nullifierHash,
+            _signal: _signal,
+            _externalNullifier: _externalNullifier,
+            _proof: _proof
+        });
+    }
+
+    /// @notice
+    function _verify(
+        uint256 _root,
+        uint256 _depth,
+        uint256 _nullifierHash,
+        uint256 _signal,
+        uint256 _externalNullifier,
+        uint256[8] calldata _proof
+    ) internal view returns (bool) {
         try SemaphoreVerifier(VERIFIER).verifyProof({
-            merkleTreeRoot: root,
+            merkleTreeRoot: _root,
             nullifierHash: _nullifierHash,
             signal: _signal,
             externalNullifier: _externalNullifier,
             proof: _proof,
-            merkleTreeDepth: depth
+            merkleTreeDepth: _depth
         }) {
             return true;
         } catch {
--- test/ZuzaluOracle.t.sol
+++ test/ZuzaluOracle.t.sol
@@ -13,21 +13,35 @@ contract ProofGeneration {
 }
 
 contract ZuzaluOracleTest is Test {
+    /// @notice
     Backend internal backend;
+    /// @notice
     ZuzaluOracle internal oracle;
-    // owner of oracle
+    /// @notice owner of the oracle
     address internal alice;
 
+    /// @notice
     function setUp() public {
         alice = makeAddr("alice");
         backend = new Backend();
         Backend.Group memory group = backend.group();
+
         address verifier = _verifier();
 
-        oracle = new ZuzaluOracle(alice, group.root, group.depth, verifier);
+        ZuzaluOracle.Commitment memory commitment = ZuzaluOracle.Commitment({
+            root: group.root,
+            depth: group.depth
+        });
+
+        oracle = new ZuzaluOracle({
+            _commitment: commitment,
+            _owner: alice,
+            _verifier: verifier
+        });
     }
 
-    function _verifier() internal returns (address) {
+    /// @notice TODO: set the code for the verifier when running locally
+    function _verifier() internal view returns (address) {
         uint256 chainid = block.chainid;
         if (chainid == 5) {
             return 0xb908Bcb798e5353fB90155C692BddE3b4937217C;
@@ -35,8 +49,39 @@ contract ZuzaluOracleTest is Test {
         revert("unknown chainid");
     }
 
-    // need to create proof
-    function test_foo() external skipWhenNotForking {
-        console.log(address(oracle));
+    function _commitment(uint256 _root, uint256 _depth) internal pure returns (ZuzaluOracle.Commitment memory) {
+        ZuzaluOracle.Commitment memory commitment = ZuzaluOracle.Commitment({
+            root: _root,
+            depth: _depth
+        });
+        return commitment;
+    }
+
+    function test_constructor() external {
+        assertEq(oracle.owner(), alice);
+        assertEq(oracle.VERIFIER(), _verifier());
+    }
+
+    function test_commitment() external {
+        ZuzaluOracle.Commitment memory commitment = _commitment(1, 2);
+        vm.prank(oracle.owner());
+        oracle.update(commitment);
+
+        ZuzaluOracle.Commitment memory got = oracle.commitment();
+
+        assertTrue(got.root == commitment.root);
+        assertTrue(got.depth == commitment.depth);
+    }
+
+    /// @notice
+    function test_updateOnlyOwner_reverts() external skipWhenNotForking {
+        ZuzaluOracle.Commitment memory commitment = _commitment(0, 0);
+
+        address bob = makeAddr("bob");
+        assertTrue(oracle.owner() != bob);
+
+        vm.prank(bob);
+        vm.expectRevert("UNAUTHORIZED");
+        oracle.update(commitment);
     }
 }
