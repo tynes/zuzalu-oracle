24a123effc90bdb35d744fa4eb4b19062d3d94c7
Odysseas.eth <odyslam@gmail.com>
2023-04-23 16:28:12 +0200

    ref: support multiple groups in test

--- ffi/getGroup.ts
+++ ffi/getGroup.ts
@@ -2,11 +2,16 @@ import { ethers } from 'ethers'
 import fetch from 'node-fetch'
 import { deserializeSemaphoreGroup } from '@pcd/semaphore-group-pcd'
 
-const url = 'https://api.pcd-passport.com/semaphore/1'
+const url = 'https://api.pcd-passport.com/semaphore/'
 const coder = ethers.AbiCoder.defaultAbiCoder()
 const abi = ['uint256', 'string', 'uint256[]', 'uint256', 'uint256']
 
 ;(async () => {
+    const group = process.argv[0]
+    if (!group) {
+        console.error('Please provide a group id')
+        process.exit(1)
+    }
     const response = await fetch(url)
     const json = await response.json()
 
